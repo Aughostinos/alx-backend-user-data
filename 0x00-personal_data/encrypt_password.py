a5eeac572e13d4a24276af996235749390f5d2f2
Aughostinos <augustinos.nabil@gmail.com>
2024-09-13 03:55:37 +0300

    task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -17,3 +17,10 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode(), salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    make sure that password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
