e60729de0ae811d4b8cf51d9bf04f34587db5cbd
Aughostinos <augustinos.nabil@gmail.com>
2024-09-22 03:52:42 +0300

    task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,25 @@
+from flask import request
+from typing import List, TypeVar
+
+class Auth:
+    """
+    Auth class to manage the API authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Public method that returns False - path
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Public method that returns None
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Public method that returns None
+        """
+        return None
