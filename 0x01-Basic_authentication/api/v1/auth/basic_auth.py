9d253e11b1b19f66704b84c26291a67391b7baea
Aughostinos <augustinos.nabil@gmail.com>
2024-09-25 21:59:30 +0300

    fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -57,7 +57,7 @@ class BasicAuth(Auth):
         return email, password
 
     def user_object_from_credentials(
-        self, user_email: str, user_pwd: str) -> TypeVar('User'):
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """ returns the User instance based on his email and password."""
         if user_email is None or not isinstance(user_email, str):
             return None
@@ -67,7 +67,7 @@ class BasicAuth(Auth):
         if not users:
             return None
         user = users[0]
-        
+
         if user.is_valid_password(user_pwd):
             return user
         return None
