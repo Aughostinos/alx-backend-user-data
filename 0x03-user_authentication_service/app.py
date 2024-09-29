ce80fe0603a5b567add3da18772c66abdf25eac9
Aughostinos <augustinos.nabil@gmail.com>
2024-09-29 18:35:29 +0300

    task 17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -80,5 +80,19 @@ def profile() -> Response:
     return jsonify({"email": user.email})
 
 
+@app.route('/reset_password', methods=['GET'])
+def get_reset_password_token() -> Response:
+    """respond to the POST /reset_password route."""
+    email = request.form.get('email')
+    if not email:
+        return jsonify({"message": "email required"}), 400
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
