00aba14e77fc928d5b948aef0ca366e31eb2758f
Aughostinos <augustinos.nabil@gmail.com>
2024-09-29 18:57:24 +0300

    fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,7 +87,7 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
-    
+
     def update_password(self, reset_token: str, password: str) -> None:
         """ find the corresponding user. If it does not exist,
         raise a ValueError exception. Otherwise, hash the password
@@ -99,10 +99,11 @@ class Auth:
             return None
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-        except ValueError:
+        except NoResultFound:
             raise ValueError("Invalid reset token")
         hashed_password = _hash_password(password)
-        self._db.update_user(user.id, hashed_password=hashed_password, reset_token=None)
+        self._db.update_user(user.id,hashed_password=hashed_password,
+                             reset_token=None)
 
 
 def _hash_password(password: str) -> bytes:
