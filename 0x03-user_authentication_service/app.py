2ec9ae05b3d6c3b5017cf744dd76cd2fae0a826b
Aughostinos <augustinos.nabil@gmail.com>
2024-09-29 18:07:06 +0300

    fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -72,11 +72,12 @@ def profile() -> Response:
     session_id = request.cookies.get('session_id')
     if not session_id:
         abort(403)
-    try:
-        user = AUTH.get_user_from_session_id(session_id)
-        return jsonify({"email": user.email})
-    except Exception:
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
         abort(403)
+
+    return jsonify({"email": user.email})
 
 
 if __name__ == "__main__":
