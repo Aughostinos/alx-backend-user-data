259e4f95324de22e6cac37a7d52ec5f84e33b3e5
Aughostinos <augustinos.nabil@gmail.com>
2024-09-29 04:47:04 +0300

    fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,13 +56,13 @@ class Auth:
     def get_user_from_session_id(self, session_id: str) -> Optional[User]:
         """It takes a single session_id string argument
         and returns the corresponding User or None"""
-        if not session_id:
+        if session_id is None:
             return None
         try:
-            user = self._db.find_user_by(session_id)
+            user = self._db.find_user_by(session_id=session_id)
+            return user
         except NoResultFound:
             return None
-        return user
 
 
 def _hash_password(password: str) -> bytes:
